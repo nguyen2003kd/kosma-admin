293ca1618b546cf55a8532f0cea45474ee11d5ac
Vu Dinh Nguyen <vudinhnguy2003@gmail.com>
2026-08-05 15:02:05 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'smeq-dev.meucorp.com',
+        hostname: 'kosmo.vietprodev.com',
         pathname: '/**',
       },
       {
