4ca659d292e94bd3057d91dd78132deb834f2f30
baophm03 <baoisme2003@gmail.com>
2026-08-11 20:03:50 +0700

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,13 +25,13 @@ export const metadata: Metadata = {
 
   title: {
     default:
-      'Kosmo - Admin',
+      'Kosmo DNC - Admin',
     template:
-      '%s | Kosmo - Admin',
+      '%s | Kosmo DNC - Admin',
   },
 
   description:
-    'Kosmo - Admin',
+    'Kosmo DNC - Admin',
 
   robots: {
     index: false,
@@ -45,12 +45,12 @@ export const metadata: Metadata = {
 
   openGraph: {
     title:
-      'Kosmo - Admin',
+      'Kosmo DNC - Admin',
     description:
-      'Kosmo - Admin',
+      'Kosmo DNC - Admin',
     url: baseConfig.frontendDomain,
     siteName:
-      'Kosmo - Admin',
+      'Kosmo DNC - Admin',
     images: [
       {
         url: `${baseConfig.frontendDomain}/${thumbnail.src}`,
@@ -67,9 +67,9 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title:
-      'Kosmo - Admin',
+      'Kosmo DNC - Admin',
     description:
-      'Kosmo - Admin',
+      'Kosmo DNC - Admin',
     images: [`${baseConfig.frontendDomain}/${thumbnail.src}`],
   },
 }
@@ -81,9 +81,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
